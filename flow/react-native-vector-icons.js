88c7c7c16c10fb944838bdb5abf5e6dc595b200e
irisAsh <043takotoka@gmail.com>
2017-11-23 17:09:08 +0900

    Install react-native-vector-icons and set up it for flow

---
+++ flow/react-native-vector-icons.js
@@ -0,0 +1,49 @@
+// @flow
+
+declare module 'react-native-vector-icons' {
+  declare var exports: any;
+}
+
+declare module 'react-native-vector-icons/Entypo' {
+  declare var exports: any;
+}
+
+declare module 'react-native-vector-icons/EvilIcons' {
+  declare var exports: any;
+}
+
+declare module 'react-native-vector-icons/Feather' {
+  declare var exports: any;
+}
+
+declare module 'react-native-vector-icons/FontAwesome' {
+  declare var exports: any;
+}
+
+declare module 'react-native-vector-icons/Foundation' {
+  declare var exports: any;
+}
+
+declare module 'react-native-vector-icons/Ionicons' {
+  declare var exports: any;
+}
+
+declare module 'react-native-vector-icons/MaterialIcons' {
+  declare var exports: any;
+}
+
+declare module 'react-native-vector-icons/MaterialCommunityIcons' {
+  declare var exports: any;
+}
+
+declare module 'react-native-vector-icons/Octicons' {
+  declare var exports: any;
+}
+
+declare module 'react-native-vector-icons/Zocial' {
+  declare var exports: any;
+}
+
+declare module 'react-native-vector-icons/SimpleLineIcons' {
+  declare var exports: any;
+}
