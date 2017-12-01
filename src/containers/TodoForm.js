0700998a2dacb2e38d944a3903c6594ebe1c4329
irisAsh <043takotoka@gmail.com>
2017-12-02 00:33:38 +0900

    Upgrade TodoForm layout

--- src/containers/TodoForm.js
+++ src/containers/TodoForm.js
@@ -11,6 +11,7 @@ import {
   View,
 } from 'react-native';
 import Octions from 'react-native-vector-icons/Octicons';
+import EvilIcons from 'react-native-vector-icons/EvilIcons';
 import * as todoFormActionCreators from '../actions/todoForm';
 import * as todoModalActionCreators from '../actions/todoModal';
 import TodoFormBar from './TodoFormBar';
@@ -55,7 +56,15 @@ const TodoForm = ({
       <TouchableWithoutFeedback onPress={() => openModal()}>
         <Octions name="calendar" size={20} />
       </TouchableWithoutFeedback>
-      <Text>{DateUtil.convertToSlashFormat(todoDate)}</Text>
+      <TextInput
+        editable={false}
+        placeholder="0000/00/00"
+        value={DateUtil.convertToSlashFormat(todoDate)}
+      />
+      <TouchableWithoutFeedback>
+        <EvilIcons name="clock" size={24} />
+      </TouchableWithoutFeedback>
+      <TextInput editable={false} placeholder="00:00" />
     </View>
   </ScrollView>
 );
