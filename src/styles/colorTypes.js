067f2e1999ca28cb4e3308d82cb4c7012ad9b495
irisAsh <043takotoka@gmail.com>
2017-12-06 01:05:02 +0900

    Add the MAGENTA color type

---
+++ src/styles/colorTypes.js
@@ -0,0 +1,8 @@
+export const MAGENTA_STYLE = {
+  backgroundColorDark: '#FF5990',
+  backgroundColorLight: '#FFD1DF',
+  textColorDefault: '#000000',
+  textColorReverse: '#FFFFFF',
+  textColorDark: '#FF5990',
+  textColorLight: '#FFD1DF',
+};
