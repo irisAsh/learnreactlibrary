c25174bd3767d3ed45b071eff78576e97c4f7b86
irisAsh <043takotoka@gmail.com>
2017-12-18 01:58:03 +0900

    Add List Screen Tab

--- src/navigations/TodoTabNavigator.js
+++ src/navigations/TodoTabNavigator.js
@@ -1,13 +1,31 @@
-import React from 'react';
+// @flow
+
+import React, { ReactElement } from 'react';
 import { TabNavigator } from 'react-navigation';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import TodoMain from '../screens/TodoMain';
+import TodoList from '../screens/TodoList';
+
+const renderTabBarIcon = (Icon: ReactElement, color: string, name: string, size: number) => (
+  <Icon color={color} name={name} size={size} />
+);
 
 const TodoTabNavigator = TabNavigator(
   {
+    TodoListTab: {
+      screen: TodoList,
+      navigationOptions: {
+        tabBarLabel: 'List',
+        tabBarIcon: ({ tintColor }) =>
+          renderTabBarIcon(MaterialCommunityIcons, tintColor, 'format-list-numbers', 24),
+      },
+    },
     TodoMainTab: {
       screen: TodoMain,
-      navigationOption: {
+      navigationOptions: {
         tabBarLabel: 'Home',
+        tabBarIcon: ({ tintColor }) =>
+          renderTabBarIcon(MaterialCommunityIcons, tintColor, 'plus-outline', 24),
       },
     },
   },
